00429f9b2ea37508bf01c34d1324dc205f39614a
crisschan <can101208@gmail.com>
2019-05-09 13:33:00 +0800

    move conect to down and change zhe histroy tab information

--- vanarus/pages/chooseLib/chooseLib.js
+++ vanarus/pages/chooseLib/chooseLib.js
@@ -1,12 +1,30 @@
 // pages/chooseLib/chooseLib.js
 const app = getApp()
+let interstitialAd = null
 Page({
 
   data: {
     url: ''
   },
+  onShow:function(){
 
+    // 在适合的场景显示插屏广告
+    if (interstitialAd) {
+      interstitialAd.show().catch((err) => {
+        console.error(err)
+      })
+    }
+  },
   onLoad: function(options) {
+    if (wx.createInterstitialAd) {
+      interstitialAd = wx.createInterstitialAd({
+        adUnitId: 'adunit-cedc5db694a00cb3'
+      })
+      interstitialAd.onLoad(() => { })
+      interstitialAd.onError((err) => { })
+      interstitialAd.onClose(() => { })
+    }
+   
     var that = this;
     app.globalData.DB.collection('varanruslist').where({
       id: options.id
